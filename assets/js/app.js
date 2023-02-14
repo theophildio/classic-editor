3509da10e3104ce08a0ebb7117295e772f48efe7
Theophil Dio <dtheo.sangma@gmail.com>
2023-02-15 00:52:32 +0600

    Text alignment js added to repo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,30 @@
 // Get all elements
-const textArea = getTextAreaValue('text-area');
+const textArea = getTextArea('text-area');
 const textBold = getElement('text-bold');
 const textItalic = getElement('text-italic');
 const textUnderline = getElement('text-underline');
+const textLeft = getElement('text-left');
+const textCenter = getElement('text-center');
+const textRight = getElement('text-right');
+const textJustify = getElement('text-justify');
 
 // Make text bold
-textBold.addEventListener('click', function(e) {
-    textDecoration('font-bold');
-})
+toggleToChanges(textBold, 'font-bold');
 
 // Make text italic
-textItalic.addEventListener('click', function(e) {
-    textDecoration('italic');
-})
+toggleToChanges(textItalic, 'italic');
 
 // Make text underline
-textUnderline.addEventListener('click', function(e) {
-    textDecoration('underline');
-})
+toggleToChanges(textUnderline, 'underline');
+
+// Make text left align
+textAlign(textLeft, textArea, 'left');
+
+// Make text center align 
+textAlign(textCenter, textArea, 'center');
+
+// Make text right align 
+textAlign(textRight, textArea, 'right');
+
+// Make text right align 
+textAlign(textJustify, textArea, 'justify');
--- assets/js/functions.js
+++ assets/js/functions.js
@@ -4,14 +4,30 @@ function getElement(id) {
     return element;
 }
 
-// Get the text area value
-function getTextAreaValue(id) {
+// Function to get the text area
+function getTextArea(id) {
     const textArea = document.getElementById(id);
     return textArea;
 }
 
-// Text decoration
+// Function to text decoration
 function textDecoration(className) {
     const textOutput =  textArea.classList.toggle(className);
     return textOutput;
+}
+
+// Function to toggle to change text decoration and style
+function toggleToChanges(element, getClassName) {
+    const output = element.addEventListener('click', function(e) {
+        textDecoration(getClassName);
+    })
+    return output;
+}
+
+// Function to text alignment
+function textAlign(element, textArea, value) {
+    const textAlign = element.addEventListener('click', function() {
+        textArea.style.textAlign = value;
+    })
+    return textAlign;
 }
